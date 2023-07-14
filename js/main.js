fe616745417f341e14fec199926a8207a4571e63
wieb0028 <wieb0028@algonquinlive.com>
2023-07-14 13:59:42 -0400

    mostly complete

--- js/main.js
+++ js/main.js
@@ -48,3 +48,22 @@ const cats = [
     img: "images/kitten8.jpeg"
   }
 ]
+
+const catsRow = document.getElementById("catsRow")
+// loop over the array of data
+for(const cat of cats) {
+  console.log(cat.name)
+  const card = `
+  <div class="col">
+    <div class="card">
+      <img data-bs-toggle="modal" data-bs-target="#exampleModal" src="${cat.thumb}" data-fullimg="${cat.img}" class="card-img-top" alt="placeholder kitten">
+      <div class="card-body">
+        <h5 class="card-title">${cat.name}</h5>
+        <p class="card-text">${cat.bio}</p>
+        <a href="#" class="btn btn-light">Like</a>
+      </div>
+    </div>
+  </div>`
+  
+  catsRow.insertAdjacentHTML("beforeend", card)
+}
